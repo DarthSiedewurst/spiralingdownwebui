0ec3c317eea27336ac4e3270e49c4d06ce937646
Pierre <prange@quarzwerke.com>
2020-06-12 21:55:01 +0200

    indexed

--- src/rules/index.js
+++ src/rules/index.js
@@ -1,4 +1,4 @@
-exports.ruleset1 = require("./roompot.json");
+exports.roompot = require("./roompot.json");
 exports.default = require("./default.json");
 exports.janschepers = require("./janschepers.json");
 exports.ruleset1 = require("./ruleset1.json");
