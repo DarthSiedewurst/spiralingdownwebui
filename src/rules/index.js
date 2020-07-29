2bc617e7a76e42866a7697d2319b8abf26d9e08d
= <=>
2020-07-30 00:22:39 +0200

    spongebob.json piberto.json

--- src/rules/index.js
+++ src/rules/index.js
@@ -1,3 +1,5 @@
 exports.RoomPot = require("./roompot.json");
 exports.SpiralingDown = require("./spiralingdown.json");
 exports.Windeck10 = require("./Windeck1.0.json");
+exports.Spongebob = require("./spongebob.json");
+exports.Piberto = require("./piberto.json");
